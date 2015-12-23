24f0e2fc2bb36acbe7818b1aca362d7108e7600a
Peter Hajas <peterhajas@gmail.com>
2015-12-23 13:56:29 -0600

    Initial commit

---
+++ ArchillectDesktop/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  ArchillectDesktop
+//
+//  Created by Peter Hajas on 12/23/15.
+//  Copyright © 2015 Peter Hajas. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
